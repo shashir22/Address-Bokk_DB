6dfc636002fc830a1c837a2ee299c7160cc03b67
Shashikumar <shashirgowdas67@gmail.com>
2022-03-25 22:23:16 +0530

    [Shashi] Add: Inserting new contact details into address book

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -15,3 +15,12 @@ create table AddressBook
  phonenumber varchar(12) not null,
  email varchar(100) not null
 );
+select * from AddressBook;
+
+--UC3-Inserting Contacts
+insert into AddressBook values
+('Shashi','M','2nd cros Rajaji Nagar','Banglore','Karnataka','58082','8123351458','Shashi@gmail.com'),
+('Ravi','M','3nd cros Jaya Nagar','Ahmadabad','Gujarat','587682','934747358','Ravi@gmail.com'),
+('Akshata','F','4nd cros Indira Nagar','Pune','MH','80534','1234567896','Akshi@gmail.com'),
+('Porvi','F','2nd cros Rajaji Nagar','Hyderabad','Andra','80453','0987654321','Porvi@gmail.com'),
+('Raja','hsjs','2nd cros Rajaji Nagar','Tirupathi','Andra','80453','0987654321','Raja@gmail.com');
