db80490423f1fe010bd06e2fbf855179a3d2196e
Shashikumar <shashirgowdas67@gmail.com>
2022-03-25 22:05:16 +0530

    [Shashi] Add:Creating attributesof new table

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,3 +1,17 @@
 --UC1-Create Database
 create database AddressBookSystem;
 use AddressBookSystem;
+
+--UC2-Creating table
+create table AddressBook
+(
+ ID int identity(1,1) primary key,
+ firstname varchar(100) not null,
+ lastname varchar(100) not null,
+ address varchar(250) not null,
+ city varchar(100) not null,
+ state varchar(100) not null,
+ zip varchar(6) not null,
+ phonenumber varchar(12) not null,
+ email varchar(100) not null
+);
